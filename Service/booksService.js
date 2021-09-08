8ae307196ae81ea96ec27be73ec3a27c3fd14b19
hleoc <informig@gmail.com>
2021-09-07 22:42:34 -0300

    Criado o endpoint para atualizar livros.

--- Service/booksService.js
+++ Service/booksService.js
@@ -13,6 +13,20 @@ const create = async (title, author, category, userId) => {
   return model.create(title, author, category, userId);
 };
 
+const update = async (id, title, author, category, registrationDate, userId) => {
+  const book = await model.getById(id);
+  if (!book) {
+    return {
+      error: true,
+      code: 'invalid_data',
+      message: 'Algo deu errado',
+      statusCode: 401,
+    };
+  }
+  return model.update(id, { title, author, category, registrationDate }, userId);
+};
+
 module.exports = {
   create,
+  update,
 };
